e4561c0ea49a60b9b80ec95aa4a7de3aba1a4860
Shivansh Mishra <shivanshmishra151@gmail.com>
2025-03-19 22:38:33 +0530

    Tested expense controller

---
+++ backend/controllers/expense.js
@@ -0,0 +1,51 @@
+
+const expenseSchema=require('../models/expenseModel')
+exports.addExpense= async (req,res)=>{
+    const {title,amount,category,description,date}=req.body
+    //use instance of incomeSchema from incomeModel
+    const expense=expenseSchema({
+        title,
+        amount,
+        category,
+        description,
+        date
+    })
+    try{
+        //validations
+        if(!title||!amount||!category||!description||!date){
+            return res.status(400).json({message:'All fields are required'})
+        }
+        if(amount<=0||!amount ==='number'){
+            return res.status(400).json({message:'Amount must be a positive number'})
+        }
+        await expense.save()
+        res.status(201).json({message:'Expense added successfully'})
+    }
+    catch(e){
+        res.status(400).send(e)
+}
+}
+
+exports.getExpenses=async(req,res)=>{
+    try{
+        const expenses=await expenseSchema.find().sort({createdAt:-1})
+        res.status(200).json(expenses)
+    }
+    catch(e){
+        res.status(400).send(e)
+    
+}
+}
+
+exports.deleteExpense=async(req,res)=>{
+    //req.params is an object in Express.js that contains the route parameters
+    const {id}=req.params // using destructuring to extract the id value from the req.params object.
+    console.log(id)
+    try{
+        await expenseSchema.findByIdAndDelete(id)
+        res.status(200).json({message:'Expense deleted successfully'})
+    }
+    catch(e){
+        res.status(400).send(e)
+    }
+}
---
+++ backend/models/expenseModel.js
@@ -0,0 +1,41 @@
+const { maxHeaderSize } = require('http')
+const mongoose=require('mongoose')
+const { type } = require('os')
+
+const expenseSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+    amount:{
+        type:Number,
+        required:true,
+        maxLength:20,
+        trim:true
+    },
+    type:{
+        type:String,
+        default:'income'
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+        required:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+},{timestamps:true})
+
+Expense= mongoose.model('Expense',expenseSchema)
+module.exports=Expense

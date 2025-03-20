641bf9729b74f317b25607624586f4f222e0ca35
Shivansh Mishra <shivanshmishra151@gmail.com>
2025-03-20 16:48:55 +0530

    correction in utils and nav

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import avatar from '../../img/avatar.png'
+import avatar from '../../image/avatar.png'
 import { signout } from '../../utils/Icons'
 import { menuItems } from '../../utils/menuItems'
 
